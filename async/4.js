b52e1d518b84118edcd7145c915a4bce13193886
Your Name <kangqi@mail.ustc.edu.cn>
2017-07-08 00:15:52 +0800

    add 4.js

--- async/4.js
+++ async/4.js
@@ -3,6 +3,42 @@ var express = require("express")
 var http = require("http")
 var async = require("async")
 
-async.series(
+var url = process.argv[2] + ":" + process.argv[3] + "/users/create"
+var url2 = process.argv[2] + ":" + process.argv[3] + "/users"
+
+
+
+
+/*async.times(5, (n, next) => {
+    console.log(n)
+    next(null)
+}, (err, result) => {
+    if (err) console.error(err)
     
-)
+})*/
+
+async.series([
+    async.times(5, (n, cb))
+    }, (err, results) => {
+
+    }),
+    (cb) => {
+        http.get(url2, (res) => {
+            var body = ""
+            res.on("data", (chunk) => {
+                body += chunk.toString()
+            })
+            res.on("end", () => {
+                cb(null, body)
+            })
+        }).on("error", (err) => {
+            cb(err)
+        })
+    }
+]
+
+
+
+, (err, result) => {
+
+})
